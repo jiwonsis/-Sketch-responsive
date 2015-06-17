98df260abd0455166b80cc33ff8820ae93d856a1
ScottMoon <jiwonsis@naver.com>
2015-06-17 18:28:04 +0900

    Creating a tween animation

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,9 +1,30 @@
 $(function(){
-	var wheight = $(window).height(); //Height of window;
+	var wheight = $(window).height(); // get height of window;
 	$('.fullheight').css('height', wheight);
 
 	$(window).resize(function(){
-		var wheight = $(window).height(); //Height of window;
+		var wheight = $(window).height(); // get height of window;
 		$('.fullheight').css('height', wheight);
+	}); // on resize
+
+
+	// setup scroll magic
+	
+	var controller = new ScrollMagic({
+		globalSceneOptions: {
+			triggerHook: 'onLeave'
+		}
 	});
-});
+
+	var attractionstween = TweenMax.staggerFromTo(
+			'#attractions article', 1, 
+			{ opacity: 0, scale: 0 } ,
+			{ delay: 1, opacity: 1, scale: 1, ease: Back.easeOut });
+
+	var scene = new ScrollScene({
+		triggerElement: '#attractions',
+
+	}).setTween(attractionstween).addTo(controller);
+
+
+}); // on load
