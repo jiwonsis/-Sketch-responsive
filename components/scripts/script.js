bcca11f165b8b28d3082bc057bc5e3b1047efae5
ScottMoon <jiwonsis@naver.com>
2015-06-17 19:21:36 +0900

    Highlighting menu items on scroll

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -10,20 +10,51 @@ $(function() {
 
 
     // https://css-tricks.com/snippets/jquery/smooth-scrolling/
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top - topOffset
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - topOffset
+                }, 1000);
+                return false;
+            } // if target.length
+        } // location hostname
+    }); // onClick
+
+
+    $(window).scroll(function(){
+    	var windowPos = $(window).scrollTop() + topOffset;
+    	$('nav li a').removeClass('active');
+
+    	if(windowPos > $('#hotelinfo').offset().top) {
+    		$('nav li a').removeClass('active');
+    		$('a[href$="#hotelinfo"]').addClass('active');
+    	} // if windowPos
+
+    	if(windowPos > $('#rooms').offset().top) {
+    		$('nav li a').removeClass('active');
+    		$('a[href$="#rooms"]').addClass('active');
+    	} // if windowPos
+
+    	if(windowPos > $('#dining').offset().top) {
+    		$('nav li a').removeClass('active');
+    		$('a[href$="#dining"]').addClass('active');
+    	} // if windowPos
+
+    	if(windowPos > $('#events').offset().top) {
+    		$('nav li a').removeClass('active');
+    		$('a[href$="#events"]').addClass('active');
+    	} // if windowPos
+
+    	if(windowPos > $('#attractions').offset().top) {
+    		$('nav li a').removeClass('active');
+    		$('a[href$="#attractions"]').addClass('active');
+    	} // if windowPos
+    }); // scroll function
+
+
 
 
     // setup scroll magic
