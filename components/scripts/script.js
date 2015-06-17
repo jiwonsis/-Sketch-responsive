5cdc6471841727c6c5f7621c454b48f0d136313a
ScottMoon <jiwonsis@naver.com>
2015-06-17 19:10:04 +0900

    Pinning your navigation and smoothing scrolling

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,30 +1,58 @@
-$(function(){
-	var wheight = $(window).height(); // get height of window;
-	$('.fullheight').css('height', wheight);
+$(function() {
+    var topOffset = 43;
+    var wheight = $(window).height(); // get height of window;
+    $('.fullheight').css('height', wheight);
 
-	$(window).resize(function(){
-		var wheight = $(window).height(); // get height of window;
-		$('.fullheight').css('height', wheight);
-	}); // on resize
+    $(window).resize(function() {
+        var wheight = $(window).height(); // get height of window;
+        $('.fullheight').css('height', wheight);
+    }); // on resize
 
 
-	// setup scroll magic
-	
-	var controller = new ScrollMagic({
-		globalSceneOptions: {
-			triggerHook: 'onLeave'
-		}
-	});
+    // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - topOffset
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 
-	var attractionstween = TweenMax.staggerFromTo(
-			'#attractions article', 1, 
-			{ opacity: 0, scale: 0 } ,
-			{ delay: 1, opacity: 1, scale: 1, ease: Back.easeOut });
 
-	var scene = new ScrollScene({
-		triggerElement: '#attractions',
+    // setup scroll magic
 
-	}).setTween(attractionstween).addTo(controller);
+    var controller = new ScrollMagic({
+        globalSceneOptions: {
+            triggerHook: 'onLeave'
+        }
+    });
+
+    var pin = new ScrollScene({
+        triggerElement: '#nav',
+    }).setPin('#nav').addTo(controller);
+
+    var attractionstween = TweenMax.staggerFromTo(
+        '#attractions article', 1, {
+            opacity: 0,
+            scale: 0
+        }, {
+            delay: 1,
+            opacity: 1,
+            scale: 1,
+            ease: Back.easeOut
+        });
+
+    var scene = new ScrollScene({
+        triggerElement: '#attractions',
+        offset: -topOffset
+    }).setTween(attractionstween).addTo(controller);
 
 
 }); // on load
